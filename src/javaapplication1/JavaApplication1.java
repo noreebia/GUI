5f561786f9c9c4b19acb110139502f615426c6ca
noreebia <noreebia@gmail.com>
2017-11-18 02:43:55 +0900

    Update

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -5,18 +5,43 @@
  */
 package javaapplication1;
 
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JApplet;
+import javax.swing.JFrame;
+
 /**
  *
  * @author noreebia
  */
 public class JavaApplication1 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        StartScreen startScreen = new StartScreen();
-        startScreen.setVisible(true);
-    }
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+		
+		StartScreen startScreen = new StartScreen();
+		startScreen.setVisible(true);
+		
+		//FrameWithAPanel frame = new FrameWithAPanel();
+		
+		
+		/*
+        JFrame f = new JFrame("ShapesDemo2D");
+        f.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {System.exit(0);}
+        });
+        JApplet applet = new ShapesDemo2D();
+        f.getContentPane().add("Center", applet);
+        applet.init();
+        f.pack();
+        f.setSize(new Dimension(550,100));
+        f.setVisible(true);
+		*/
+	}
 }
